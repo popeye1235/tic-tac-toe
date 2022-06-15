290a0f0981ea704611f8f4db90f71189e186e68a
sudip mishra <sudip.mishra@pw.live>
2022-06-16 01:48:22 +0530

    tic-tac-toe

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+})
+export class SquareComponent {
+
+  @Input()
+  value!: 'X' | 'O';
+
+
+}
